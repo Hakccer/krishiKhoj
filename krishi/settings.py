adad74f274af70d263669901d045f5aeab299505
Hakccer <81087743+Hakccer@users.noreply.github.com>
2022-11-14 00:05:52 +0530

    Update settings.py

--- krishi/settings.py
+++ krishi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-c_fl9xpm1rhpnf*(j+(^9ne1ay7#l+h%$30^h9i2&m%s8%bbon
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', "sample.domain.com"]
+ALLOWED_HOSTS = ['127.0.0.1', "krishikhojapp.herokuapp.com"]
 
 
 # Application definition
